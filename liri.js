d534a3964b498bb8f153330c8a383443c8eca3a8
thechutrain <chu.chuyalan@gmail.com>
2017-01-23 12:49:17 -0500

    modularizing my getMovie script into its own lib

--- liri.js
+++ liri.js
@@ -1,7 +1,9 @@
-var request = require('request');
+// var request = require('request');
 var Twitter = require('twitter');
 var Spotify = require("spotify");
 var TwitterKeys = require("./keys.js");
+
+var Movie = require("./lib/getMovie.js");
 
 
 // get twitter keys
@@ -20,7 +22,7 @@ switch (cmd) {
         break;
     case "movie-this":
         console.log("Getting you the movie ..." + userInput);
-        getMovie(userInput);
+        Movie.getMovie(userInput); // custom function in my lib folder
         break;
     case "do-what-it-says":
         console.log("doing what it says? ..");
@@ -30,48 +32,3 @@ switch (cmd) {
         console.log("[options] 'my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says' ")
         break;
 };
-
-// -------- Main FUNCTIONS ------
-function getMovie(search){
-    var searchTitle = search;
-    // if undefined search, set it to Mr. Body
-    if (search == undefined) var searchTitle = "Mr. Nobody";
-    
-    // build request url
-    var re = new RegExp("/[\s]/","g");
-    var search_term = searchTitle.replace(re, "+");
-    // var search_term = searchTitle.split(" ").join("+"); // two different ways, but can't do .replace(" ", "+") --> only one match max
-    console.log(search_term);
-    const base_url = "http://www.omdbapi.com/?";
-    var search_url = "y=&plot=short&r=json&tomatoes=true&t=" + search_term;
-    // make request
-    request(base_url + search_url, function(error, response, body){
-        if (!error && response.statusCode == 200){
-            // console.log(body);
-            var body = JSON.parse(body);
-            var data = {
-            "title": body["Title"],
-            "year": body["Year"],
-            "IMDB rating": body["imdbRating"],
-            "country": body["Country"],
-            "language" : body["Language"],
-            "plot" : body["Plot"],
-            "actors" : body["Actors"],
-            "Rotten Tomatoes Rating" : body["tomatoRating"],
-            "Rotten Tomatoes url" : body["tomatoURL"],
-            }; 
-            // console.log(data);
-            prettyPrintData(data);
-        }
-    }); // request
-
-}; // getMovie
-
-
-
-// -------- helper FUNCTIONS ------
-function prettyPrintData(object){
-    for (key in object){
-        console.log(`${key}: ${object[key]}`);
-    }
-}
